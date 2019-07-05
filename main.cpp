864deca5a7c8055f0222618ef1227d392a32dd49
Alex Yang <aleozlx@gmail.com>
2019-07-05 00:16:47 -0500

    more histograms

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "imgui_impl_opengl3.h"
 #include <iostream>
 #include <string>
+#include <vector>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <SOIL/SOIL.h>
@@ -201,6 +202,75 @@ void spotlight(cv::OutputArray _frame, cv::InputArray _sel, float alpha) {
     }
 }
 
+struct RGBHistogram {
+    float alpha;
+    unsigned int width, height;
+    std::vector<cv::Mat> bgr_planes;
+
+    RGBHistogram(cv::InputArray inputRGBImage, unsigned int width, unsigned int height, float alpha=1.0f) {
+        this->width = width;
+        this->height = height;
+        this->alpha = alpha;
+        cv::split(inputRGBImage, bgr_planes);
+    }
+
+    void Compute(cv::OutputArray output, cv::InputArray mask=cv::noArray()) {
+        const int histSize = 256;
+        const float range[] = { 0, 256 } ;
+        const float* histRange = { range };
+        bool uniform = true; bool accumulate = false;
+        cv::Mat b_hist, g_hist, r_hist;
+        cv::calcHist(&bgr_planes[0], 1, 0, cv::noArray(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+        cv::calcHist(&bgr_planes[1], 1, 0, cv::noArray(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+        cv::calcHist(&bgr_planes[2], 1, 0, cv::noArray(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
+
+        // Draw the global histograms for B, G and R
+        int bin_w = cvRound((double) width/histSize);
+        cv::Mat histImage(height, width, CV_8UC3, cv::Scalar(0,0,0));
+
+        /// Normalize the result to [ 0, histImage.rows ]
+        cv::normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::noArray());
+        cv::normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::noArray());
+        cv::normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::noArray());
+
+        // Draw for each channel
+        // Note: Blue and Red channels are purposefully swapped to avoid color conversion before rendering
+        float _alpha = mask.empty()?1.0f:this->alpha;
+        for(int i = 1;i < histSize;i++){
+            cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(b_hist.at<float>(i-1)) ) ,
+                            cv::Point( bin_w*(i), height - cvRound(b_hist.at<float>(i)) ),
+                            cv::Scalar( 0, 0, 255*_alpha), 2, 8, 0  );
+            cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(g_hist.at<float>(i-1)) ) ,
+                            cv::Point( bin_w*(i), height - cvRound(g_hist.at<float>(i)) ),
+                            cv::Scalar( 0, 255*_alpha, 0), 2, 8, 0  );
+            cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(r_hist.at<float>(i-1)) ) ,
+                            cv::Point( bin_w*(i), height - cvRound(r_hist.at<float>(i)) ),
+                            cv::Scalar( 255*_alpha, 0, 0), 2, 8, 0  );
+        }
+
+        if (!mask.empty()) {
+            cv::calcHist(&bgr_planes[0], 1, 0, mask, b_hist, 1, &histSize, &histRange, uniform, accumulate);
+            cv::calcHist(&bgr_planes[1], 1, 0, mask, g_hist, 1, &histSize, &histRange, uniform, accumulate);
+            cv::calcHist(&bgr_planes[2], 1, 0, mask, r_hist, 1, &histSize, &histRange, uniform, accumulate);
+            // cv::normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+            // cv::normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+            // cv::normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+            for(int i = 1;i < histSize;i++){
+                cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(b_hist.at<float>(i-1)) ) ,
+                                cv::Point( bin_w*(i), height - cvRound(b_hist.at<float>(i)) ),
+                                cv::Scalar( 0, 0, 255), 2, 8, 0  );
+                cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(g_hist.at<float>(i-1)) ) ,
+                                cv::Point( bin_w*(i), height - cvRound(g_hist.at<float>(i)) ),
+                                cv::Scalar( 0, 255, 0), 2, 8, 0  );
+                cv::line( histImage, cv::Point( bin_w*(i-1), height - cvRound(r_hist.at<float>(i-1)) ) ,
+                                cv::Point( bin_w*(i), height - cvRound(r_hist.at<float>(i)) ),
+                                cv::Scalar( 255, 0, 0), 2, 8, 0  );
+            }
+        }
+        output.assign(histImage);
+    }
+};
+
 int main(int, char**) {
     App app = App::Initialize();
     if (!app.ok) return 1;
@@ -218,7 +288,9 @@ int main(int, char**) {
     cam.capture >> frame;
     cv::Size frame_size = frame.size();
     int width=frame_size.width, height=frame_size.height, channels=3;
-    TexImage my_tex(width, height, channels);
+    TexImage imSuperpixels(width, height, channels);
+    TexImage imHistogram(432, 400, 3);
+    cv::Mat histogram, histogram_rgb;
 
     // Main loop
     while (!glfwWindowShouldClose(window)){
@@ -251,25 +323,25 @@ int main(int, char**) {
             if (use_spotlight) spotlight(frame_rgb, superpixel_selected, 0.5);
             frame_rgb.setTo(cv::Scalar(200, 5, 240), superpixel_contour);
             
-            my_tex.Load(frame_rgb.data);
-            ImGui::Text("(%d, %d) => (%d,)", my_tex.width, my_tex.height, superpixel->GetNumSuperpixels());
+            imSuperpixels.Load(frame_rgb.data);
+            ImGui::Text("(%d, %d) => (%d,)", imSuperpixels.width, imSuperpixels.height, superpixel->GetNumSuperpixels());
             ImVec2 pos = ImGui::GetCursorScreenPos();
-            ImGui::Image(my_tex.id(), my_tex.size(), ImVec2(0,0), ImVec2(1,1), ImVec4(1.0f,1.0f,1.0f,1.0f), ImVec4(1.0f,1.0f,1.0f,0.5f));
+            ImGui::Image(imSuperpixels.id(), imSuperpixels.size(), ImVec2(0,0), ImVec2(1,1), ImVec4(1.0f,1.0f,1.0f,1.0f), ImVec4(1.0f,1.0f,1.0f,0.5f));
             if (ImGui::IsItemHovered()) {
                 pointer_x = static_cast<int>(io.MousePos.x - pos.x);
                 pointer_y = static_cast<int>(io.MousePos.y - pos.y);
                 if (use_magnifier) {
                     ImGui::BeginTooltip();
                     float region_sz = 32.0f;
-                    float region_x = io.MousePos.x - pos.x - region_sz * 0.5f; if (region_x < 0.0f) region_x = 0.0f; else if (region_x > my_tex.f32width - region_sz) region_x = my_tex.f32width - region_sz;
-                    float region_y = io.MousePos.y - pos.y - region_sz * 0.5f; if (region_y < 0.0f) region_y = 0.0f; else if (region_y > my_tex.f32height - region_sz) region_y = my_tex.f32height - region_sz;
+                    float region_x = io.MousePos.x - pos.x - region_sz * 0.5f; if (region_x < 0.0f) region_x = 0.0f; else if (region_x > imSuperpixels.f32width - region_sz) region_x = imSuperpixels.f32width - region_sz;
+                    float region_y = io.MousePos.y - pos.y - region_sz * 0.5f; if (region_y < 0.0f) region_y = 0.0f; else if (region_y > imSuperpixels.f32height - region_sz) region_y = imSuperpixels.f32height - region_sz;
                     float zoom = 4.0f;
                     ImGui::Text("Ptr: (%d,%d) Id: %d", pointer_x, pointer_y, superpixel_id);
                     ImGui::Text("Mean: (%.1f,%.1f,%.1f)", sel_mean[0], sel_mean[1], sel_mean[2]);
                     ImGui::Text("Std: (%.1f,%.1f,%.1f)", sel_std[0], sel_std[1], sel_std[2]);
                     ImGui::Image(
-                        my_tex.id(), ImVec2(region_sz * zoom, region_sz * zoom),
-                        my_tex.uv(region_x, region_y), my_tex.uv(region_x + region_sz, region_y + region_sz),
+                        imSuperpixels.id(), ImVec2(region_sz * zoom, region_sz * zoom),
+                        imSuperpixels.uv(region_x, region_y), imSuperpixels.uv(region_x + region_sz, region_y + region_sz),
                         ImVec4(1.0f, 1.0f, 1.0f, 1.0f), ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
                     ImGui::EndTooltip();
                 }
@@ -278,6 +350,12 @@ int main(int, char**) {
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::Checkbox("Spotlight", &use_spotlight); ImGui::SameLine(150);
             ImGui::Checkbox("Magnifier", &use_magnifier);
+
+            RGBHistogram hist(frame, imHistogram.width, imHistogram.height, (use_spotlight?0.3f:1.0f));
+            hist.Compute(histogram, use_spotlight?superpixel_selected:cv::noArray());
+            // cv::cvtColor(histogram, histogram_rgb, cv::COLOR_BGR2RGB);
+            imHistogram.Load(histogram.data);
+            ImGui::Image(imHistogram.id(), imHistogram.size(), ImVec2(0,0), ImVec2(1,1), ImVec4(1.0f,1.0f,1.0f,1.0f), ImVec4(1.0f,1.0f,1.0f,0.5f));
             ImGui::End();
         }
         app.Render(clear_color);
