d1f88f6b355a6f07bc02ce6ac72084a546c3d3c2
Alex Yang <aleozlx@gmail.com>
2019-07-14 23:27:28 -0500

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 #include <list>
@@ -13,6 +14,12 @@
 #include "teximage.hpp"
 #include "superpixel_pipeline.hpp"
 #include "misc_ocv.hpp"
+
+#ifdef HAS_TF
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/core/platform/env.h>
+namespace tf = tensorflow;
+#endif
 
 void spotlight(cv::OutputArray _frame, cv::InputArray _sel, float alpha) {
     cv::Mat frame = _frame.getMat(), selection = _sel.getMat();
@@ -111,6 +118,70 @@ struct RGBHistogram {
             }
         }
         output.assign(histImage);
+    }
+};
+
+struct DCNNInference {
+    void Compute() {
+        tf::Session* session;
+        tf::Status status = tf::NewSession(tf::SessionOptions(), &session);
+        if (!status.ok()) {
+            std::cout << status.ToString() << "\n";
+            return;
+        }
+        tf::GraphDef graph_def;
+        status = tf::ReadBinaryProto(tf::Env::Default(), "models/graph.pb", &graph_def);
+        if (!status.ok()) {
+            std::cout << status.ToString() << "\n";
+            return;
+        }
+
+        // Add the graph to the session
+        status = session->Create(graph_def);
+        if (!status.ok()) {
+            std::cout << status.ToString() << "\n";
+            return;
+        }
+
+        // Setup inputs and outputs:
+
+        // Our graph doesn't require any inputs, since it specifies default values,
+        // but we'll change an input to demonstrate.
+        tf::Tensor a(tf::DT_FLOAT, tf::TensorShape());
+        a.scalar<float>()() = 3.0;
+
+        tf::Tensor b(tf::DT_FLOAT, tf::TensorShape());
+        b.scalar<float>()() = 2.0;
+
+        std::vector<std::pair<std::string, tensorflow::Tensor>> inputs = {
+            { "a", a },
+            { "b", b },
+        };
+
+        // The session will initialize the outputs
+        std::vector<tf::Tensor> outputs;
+
+        // Run the session, evaluating our "c" operation from the graph
+        status = session->Run(inputs, {"c"}, {}, &outputs);
+        if (!status.ok()) {
+            std::cout << status.ToString() << "\n";
+            return;
+        }
+
+        // Grab the first output (we only evaluated one graph node: "c")
+        // and convert the node to a scalar representation.
+        auto output_c = outputs[0].scalar<float>();
+
+        // (There are similar methods for vectors and matrices here:
+        // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/public/tensor.h)
+
+        // Print the results
+        std::cout << outputs[0].DebugString() << "\n"; // Tensor<type: float shape: [] values: 30>
+        std::cout << output_c() << "\n"; // 30
+
+        // Free any resources used by the session
+        session->Close();
+
     }
 };
 
@@ -373,6 +444,10 @@ int main(int, char**) {
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     cameras = cv_misc::camera_enumerate2();
     windows.push_back(std::make_unique<PipelineSettingsWindow>());
+
+    DCNNInference  dcnn;
+    dcnn.Compute();
+
     while (!glfwWindowShouldClose(window)){
         glfwPollEvents();
         ImGui_ImplOpenGL3_NewFrame();
