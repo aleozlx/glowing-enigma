c0c188c6c7ba282d0506b2f7004eb6a11bb0bdeb
Alex Yang <zy5f9@mail.missouri.edu>
2019-09-05 14:27:09 -0500

    test_stream_to

--- tests/test_pq.cpp
+++ tests/test_pq.cpp
@@ -25,22 +25,20 @@ BOOST_AUTO_TEST_CASE(test_pq_conn) {
 }
 
 #define dtoa_(fp, dest) fpconv_dtoa(fp, dest)
+#define MAX_LEN_DTOA (24)
+#define MAX_DIM_FEATURE (4096)
 
 template<typename F>
 size_t vec2str(size_t dim, F *vec, char *dst) {
     if(dim<=0) return 0;
     char *dst0 = dst;
-    const char header[] = "array[";
-    std::strcpy(dst, header);
-    dst += std::strlen(header);
-    size_t len;
+    *dst++ = '{';
     do {
-        len = dtoa_(static_cast<double>(*vec), dst);
-        dst[len] = ',';
-        dst += len+1;
+        dst += dtoa_(static_cast<double>(*vec), dst);
+        *dst++ = ',';
         vec++;
     } while(--dim);
-    *(dst-1) = ']';
+    *(dst-1) = '}';
     *dst = '\0';
     return dst-dst0;
 }
@@ -53,27 +51,42 @@ BOOST_AUTO_TEST_CASE(test_stream_to) {
         {1.0, 0.0, 1.0},
     };
     std::vector<double> ff {1.0, 2.0, 1.0};
-    std::tuple<std::string, std::vector<double>> r = {"a", ff};
     std::string feature_buffer;
     try {
         pqxx::connection conn(connection);
-        pqxx::work cur(conn);
+        conn.prepare("sql_ct", "select count(*) from test_stream");
+        int ct0, ct1;
+        {
+            pqxx::work w_ct(conn);
+            pqxx::result r = w_ct.exec_prepared("sql_ct");
+            r.at(0).at(0).to(ct0);
+            w_ct.commit();
+        }
+        pqxx::work w_stream(conn);
         pqxx::stream_to s {
-            cur, "test_stream",
+            w_stream, "test_stream",
             std::vector<std::string> {
                 "name", "feature"
             }
         };
         for(auto const &feature: features) {
-            feature_buffer.resize(60*4096+20);
+            feature_buffer.resize(MAX_LEN_DTOA*MAX_DIM_FEATURE+20);
             size_t len = vec2str(3, feature.data(), const_cast<char*>(feature_buffer.data()));
             feature_buffer.resize(len);
-//            std::cout<<feature_buffer<<std::endl;
+            s<<std::make_tuple(name, feature_buffer);
         }
         s.complete();
-        cur.commit();
+        w_stream.commit();
+        {
+            pqxx::work w_ct(conn);
+            pqxx::result r = w_ct.exec_prepared("sql_ct");
+            r.at(0).at(0).to(ct1);
+            w_ct.commit();
+        }
+        BOOST_TEST(ct1-ct0 == features.size());
     }
     catch (const std::exception &e) {
         std::cerr<<e.what()<<std::endl;
+        BOOST_TEST(false);
     }
 }
