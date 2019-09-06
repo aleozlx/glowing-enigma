257cf1afa4dd3c7591fc3ac571751a8a80830dfa
Alex Yang <zy5f9@mail.missouri.edu>
2019-09-06 16:22:32 -0500

    +glob

--- src/process.cpp
+++ src/process.cpp
@@ -10,6 +10,10 @@
 #include "dcnn.hpp"
 #include "saver.hpp"
 
+extern "C" {
+#include <glob.h>
+}
+
 namespace fs = std::filesystem;
 
 void init() {
@@ -56,16 +60,14 @@ void init() {
     // std::cout << std::endl;
 }
 
-int main(int argc, char* argv[]) {
-    std::string dataset = "/tank/datasets/research/xView";
-    std::string fname = "/tank/datasets/research/xView/train_images/1036.tif";
+void process_tif(const std::string &dataset, const std::string &fname) {
     cv::Mat frame_raw = cv::imread(fname, cv::IMREAD_COLOR);
     cv::Size real_size = frame_raw.size();
     const int width = 256, height = 256, size_class = 32;
     spt::dnn::Chipping chips(real_size, cv::Size(width, height), 0.5); // TODO overlap as argument
 
     spt::dnn::VGG16SP dcnn;
-    dcnn.Summary();
+//    dcnn.Summary();
     if(dcnn.NewSession()) {
         std::cerr<<"Successfully initialized a new TensorFlow session."<<std::endl;
     }
@@ -75,31 +77,35 @@ int main(int argc, char* argv[]) {
     dcnn.SetInputResolution(256, 256);
 
     spt::GSLIC _superpixel({
-        .img_size = { width, height },
-        .no_segs = 64,
-        .spixel_size = size_class,
-        .no_iters = 5,
-        .coh_weight = 0.6f,
-        .do_enforce_connectivity = true,
-        .color_space = gSLICr::CIELAB,
-        .seg_method = gSLICr::GIVEN_SIZE
-    });
+                                   .img_size = { width, height },
+                                   .no_segs = 64,
+                                   .spixel_size = size_class,
+                                   .no_iters = 5,
+                                   .coh_weight = 0.6f,
+                                   .do_enforce_connectivity = true,
+                                   .color_space = gSLICr::CIELAB,
+                                   .seg_method = gSLICr::GIVEN_SIZE
+                           });
 
     try{
         pqxx::connection conn("dbname=xview user=postgres");
         fs::path pthFname(fname);
         conn.prepare("sql_find_frame_id", "select id from frame where image = $1");
         conn.prepare("sql_match_bbox2", // (image, cx, cy)
-"select image, class_label.label_name, bbox.xview_type_id, bbox.xview_bounds_imcoords \
+                     "select image, class_label.label_name, bbox.xview_type_id, bbox.xview_bounds_imcoords \
 from frame join bbox on frame.id = bbox.frame_id join class_label on bbox.xview_type_id = class_label.id \
 where frame.image = $1 and st_point($2, $3) && bbox.xview_bounds_imcoords \
 order by st_area(bbox.xview_bounds_imcoords);");
         pqxx::work w_frame(conn);
         std::string image = fs::path(fname).lexically_relative(dataset).string();
+        std::cerr<<"Looking up frame_id for "<<image<<std::endl;
         pqxx::result r = w_frame.exec_prepared("sql_find_frame_id", image);
         w_frame.commit();
-        if(r.size() == 0)
-            return 1;
+        if(r.size() == 0) {
+            std::cerr<<"frame_id not found."<<std::endl;
+            return;
+        }
+
         int frame_id = r[0][0].as<int>();
 
         cv::Mat frame, superpixel_labels, superpixel_selected, frame_dcnn;
@@ -122,18 +128,18 @@ order by st_area(bbox.xview_bounds_imcoords);");
         pqxx::connection conn2("dbname=xview user=postgres");
         pqxx::work w_spstream(conn2);
         pqxx::stream_to sps {
-            w_spstream, "superpixel_inference",
-            std::vector<std::string> {
-                "frame_id",
-                "size_class",
-                "area",
-                "centroid_abs_x",
-                "centroid_abs_y",
-                "dcnn_name",
-                "dcnn_feature",
-                "class_label",
-                "class_label_multiplicity"
-            }
+                w_spstream, "superpixel_inference",
+                std::vector<std::string> {
+                        "frame_id",
+                        "size_class",
+                        "area",
+                        "centroid_abs_x",
+                        "centroid_abs_y",
+                        "dcnn_name",
+                        "dcnn_feature",
+                        "class_label",
+                        "class_label_multiplicity"
+                }
         };
         std::string dcnn_name = "VGG16SP_test";
         int rows_inserted = 0;
@@ -153,10 +159,10 @@ order by st_area(bbox.xview_bounds_imcoords);");
                 cv::findContours(superpixel_selected, superpixel_sel_contour, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
                 superpixel_moments = cv::moments(superpixel_sel_contour[0], true);
-                #define v0 superpixel_moments.m00
-                #define v1 superpixel_moments.mu02
-                #define v2 superpixel_moments.mu20
-                #define v3 superpixel_moments.mu11
+#define v0 superpixel_moments.m00
+#define v1 superpixel_moments.mu02
+#define v2 superpixel_moments.mu20
+#define v3 superpixel_moments.mu11
                 if (v0 > 0) {
                     dcnn.GetFeature(s, superpixel_feature_buffer.data());
                     float cxf32 = superpixel_moments.m10/v0+roi.x, cyf32 = superpixel_moments.m01/v0+roi.y;
@@ -184,7 +190,7 @@ order by st_area(bbox.xview_bounds_imcoords);");
 //                        std::cout<<std::endl;
                     }
                 }
-                
+
             }
         }
         sps.complete();
@@ -194,5 +200,44 @@ order by st_area(bbox.xview_bounds_imcoords);");
     catch (const std::exception &e) {
         std::cerr<<e.what()<<std::endl;
     }
+}
+
+class Glob {
+protected:
+    glob_t glob_result;
+    bool _error = false;
+public:
+    Glob(const char *pattern) {
+        std::memset(&glob_result, 0, sizeof(glob_result));
+        if(glob(pattern, GLOB_TILDE, NULL, &glob_result)!=0) {
+            std::cerr << "glob() error" << std::endl;
+            _error = true;
+        }
+    }
+
+    ~Glob() {
+        globfree(&glob_result);
+    }
+
+    size_t size() {
+        return _error ? 0 : glob_result.gl_pathc;
+    }
+
+    const char* operator[](int i) {
+        return glob_result.gl_pathv[i];
+    }
+};
+
+int main(int argc, char* argv[]) {
+    std::string dataset = "/tank/datasets/research/xView";
+//    std::string fname = "/tank/datasets/research/xView/train_images/1036.tif";
+
+    Glob train_images("/tank/datasets/research/xView/train_images/3*.tif");
+    for(size_t i = 0; i < train_images.size(); ++i) {
+        std::string fname(train_images[i]);
+        std::cout<<"Processing "<<fname<<std::endl;
+        process_tif(dataset, fname);
+    }
+
     return 0;
 }
