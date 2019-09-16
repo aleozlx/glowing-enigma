d05e751a5718a6310648226e0cef1e1c52dbae9c
Alex Yang <aleozlx@gmail.com>
2019-09-16 15:33:52 -0500

    ..

--- cmake/modules/FindFilesystem.cmake
+++ cmake/modules/FindFilesystem.cmake
@@ -113,7 +113,7 @@ set(CMAKE_REQUIRED_QUIET ${Filesystem_FIND_QUIETLY})
 
 # All of our tests required C++17 or later
 set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_REQUIRED_FLAGS -std=c++17)
+set(CMAKE_REQUIRED_FLAGS -std=c++17) # depends on the cmake version the above may not work with check_cxx_source_compiles()
 
 # Normalize and check the component list we were given
 set(want_components ${Filesystem_FIND_COMPONENTS})
